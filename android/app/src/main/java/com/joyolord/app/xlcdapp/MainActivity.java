c3ba7439ff07cecc0f35fafe7a8aae7e2f56be6d
chiachang100 <chiachang100@gmail.com>
2024-02-20 23:51:22 -0800

    Check in xlcdapp with Ionic Angular

---
+++ android/app/src/main/java/com/joyolord/app/xlcdapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.joyolord.app.xlcdapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.joyolord.app.xlcdapp',
+  appName: 'xlcdapp',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
