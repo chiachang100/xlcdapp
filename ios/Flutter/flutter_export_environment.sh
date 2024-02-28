4d166037de37fd96278ffac43e289eef33b7ed68
chiachang100 <chiachang100@gmail.com>
2024-02-27 16:42:48 -0800

    Update package version

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Tools\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\src\git\chiachang100\xlcdapp"
+export "FLUTTER_APPLICATION_PATH=c:\src\git\chiachang100\xlcdapp"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.7.6"
-export "FLUTTER_BUILD_NUMBER=0.7.6"
+export "FLUTTER_BUILD_NAME=0.9.1"
+export "FLUTTER_BUILD_NUMBER=0.9.1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
