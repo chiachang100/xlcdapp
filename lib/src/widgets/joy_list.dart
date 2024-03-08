f35bee8cb0c25697ba82273546a2483a65dacb43
chiachang100 <chiachang100@gmail.com>
2024-03-08 14:08:44 -0800

    Replace lists with cards

--- lib/src/widgets/joy_list.dart
+++ lib/src/widgets/joy_list.dart
@@ -29,7 +29,63 @@ class JoyList extends StatelessWidget {
   @override
   Widget build(BuildContext context) => ListView.builder(
         itemCount: joys.length,
-        itemBuilder: (context, index) => ListTile(
+        itemBuilder: (context, index) => Card(
+          color: Colors.yellow[50],
+          elevation: 8.0,
+          margin: const EdgeInsets.all(8.0),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset(
+                  joys[index].photoUrl,
+                  height: MediaQuery.of(context).size.width * (3 / 4),
+                  width: MediaQuery.of(context).size.width,
+                  //height: 120, width: 640,
+                  fit: BoxFit.scaleDown,
+                ),
+              ),
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: circleAvatarBgColor[
+                        (joys[index].id % circleAvatarBgColor.length)],
+                    child: Text(
+                      joys[index].title.substring(0, 1),
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ),
+                  Text(
+                    joys[index].title,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                '${joys[index].scripture.verse}(${joys[index].scripture.name})',
+                style: const TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: (onTap != null ? () => onTap!(joys[index]) : null),
+                child: const Text(
+                  '看內容',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+/*         itemBuilder: (context, index) => ListTile(
           title: Text(
             joys[index].title,
           ),
@@ -48,5 +104,6 @@ class JoyList extends StatelessWidget {
           //trailing: const Icon(Icons.arrow_forward_ios),
           onTap: onTap != null ? () => onTap!(joys[index]) : null,
         ),
+ */
       );
 }
